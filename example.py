c2f63e072885311874539b07237cc2839313f955
Kye <kye@apacmediasolutions.com>
2023-08-28 16:30:29 -0400

    exampleg

--- example.py
+++ example.py
@@ -1,11 +1,9 @@
 import torch
-from kosmos.model import Kosmos2
+from nevax import Kosmos2
 
-#usage
 #usage
 img = torch.randn(1, 3, 256, 256)
-text = torch.randint(0, 20000, (1, 1024))
-
+caption_tokens = torch.randint(0, 4)
 
 model = Kosmos2()
-output = model(text, img)
+output = model(img, caption_tokens)
