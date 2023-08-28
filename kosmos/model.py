7af52be81473343d3a15dcc3e02087364bde0e18
Kye <kye@apacmediasolutions.com>
2023-08-28 16:26:47 -0400

    no pretrained module yet

--- kosmos/model.py
+++ kosmos/model.py
@@ -1,12 +1,9 @@
 import logging
 
 import torch
-import torch.nn as nn
-from flamingo_pytorch import PerceiverResampler
-from torch.nn import Module
-from transformers import AutoTokenizer, CLIPModel, CLIPProcessor
+from transformers import AutoTokenizer, CLIPProcessor
 
-from kosmos.transformer import AutoregressiveWrapper, Decoder, Transformer
+from kosmos.transformer import Decoder, Transformer, ViTransformerWrapper, Encoder
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -74,118 +71,59 @@ class Kosmos2Tokenizer:
 
 
 
-class Kosmos2(Module):
-    """
-    Andromeda is a transformer-based model architecture. It initializes with 
-    a Transformer and AutoregressiveWrapper with default or user-specified parameters.
-    """
+class Kosmos2(torch.nn.Module):
     def __init__(self, 
-                 num_tokens=50432, 
-                 max_seq_len=8192, 
-                 dim=2560, 
-                 depth=32, 
-                 dim_head=128, 
-                 heads=24,
-                 use_abs_pos_emb=False, 
-                 alibi_pos_bias=True, 
-                 alibi_num_heads=12, 
+                 image_size=256, 
+                 patch_size=32, 
+                 encoder_dim=512, 
+                 encoder_depth=6, 
+                 encoder_heads=8,
+                 num_tokens=20000, 
+                 max_seq_len=1024, 
+                 decoder_dim=512, 
+                 decoder_depth=6, 
+                 decoder_heads=8, 
+                 alibi_num_heads=4,
+                 use_abs_pos_emb=False,
+                 cross_attend=True,
+                 alibi_pos_bias=True,
                  rotary_xpos=True,
-                 attn_flash=True, 
-                #  attn_kv_heads = 2,
-                 qk_norm=True, 
-                 attn_qk_norm=True, 
-                 attn_qk_norm_dim_scale=True, 
-                 ):
-        """
-        Initialize the model with specified or default parameters.
-        Args:
-        - num_tokens: Number of tokens in the vocabulary
-        - max_seq_len: Maximum sequence length
-        - dim: Dimension of the model
-        - depth: Depth of the model
-        - dim_head: Dimension of the model head
-        - heads: Number of heads
-        - use_abs_pos_emb: Whether to use absolute position embedding
-        - alibi_pos_bias: Alibi position bias
-        - alibi_num_heads: Number of alibi heads
-        - rotary_xpos: Rotary position
-        - attn_flash: Attention flash
-        - deepnorm: Deep normalization
-        - shift_tokens: Number of tokens to shift
-        - attn_one_kv_head: Attention one key/value head
-        - qk_norm: Query-key normalization
-        - attn_qk_norm: Attention query-key normalization
-        - attn_qk_norm_dim_scale: Attention query-key normalization dimension scale
-        - embedding_provider: Embedding provider module
-        """
-        super().__init__()
-
-        try:
-            self.clip_model = CLIPModel.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K").vision_model
-        except Exception as e:
-            logging.error(f"Failed to initialize CLIP model: {e}")
-            raise
-
-        self.output_projection = nn.Linear(2048, 32002, bias=False)
-        nn.init.normal_(self.output_projection.weight, mean=0, std=2048**-0.5)
-
-        self.perceive = PerceiverResampler(
-            dim=1024, 
-            depth=2,
-            dim_head=64,
-            heads=8,
-            num_latents=64,
-            num_media_embeds=257
-        )
-        self.image_proj = nn.Linear(1024, 2048, bias=False)
-        nn.init.normal_(self.image_proj.weight, mean=0, std=2048**-0.5)
-
-
-
-        try:
-            self.kosmos = Transformer(
-                num_tokens=num_tokens,
-                max_seq_len=max_seq_len,
-                use_abs_pos_emb=use_abs_pos_emb,
-                attn_layers=Decoder(
-                    dim=dim,
-                    depth=depth,
-                    dim_head=dim_head,
-                    heads=heads,
-                    cross_attend=True,
-                    alibi_pos_bias=alibi_pos_bias,
-                    alibi_num_heads=alibi_num_heads,
-                    rotary_xpos=rotary_xpos,
-                    attn_flash=attn_flash,
-                    # attn_kv_heads=attn_kv_heads,
-                    qk_norm=qk_norm,
-                    attn_qk_norm=attn_qk_norm,
-                    attn_qk_norm_dim_scale=attn_qk_norm_dim_scale
-                )
+                 attn_flash=True,
+                 qk_norm=True):
+        
+        super(Kosmos2, self).__init__()
+        
+        self.encoder = ViTransformerWrapper(
+            image_size=image_size,
+            patch_size=patch_size,
+            attn_layers=Encoder(
+                dim=encoder_dim,
+                depth=encoder_depth,
+                heads=encoder_heads
             )
+        )
 
-            self.decoder = AutoregressiveWrapper(self.kosmos)
+        self.decoder = Transformer(
+            num_tokens=num_tokens,
+            max_seq_len=max_seq_len,
+            use_abs_pos_emb=use_abs_pos_emb,
+            attn_layers=Decoder(
+                dim=decoder_dim,
+                depth=decoder_depth,
+                heads=decoder_heads,
+                cross_attend=cross_attend,
+                alibi_pos_bias=alibi_pos_bias,
+                alibi_num_heads=alibi_num_heads,
+                rotary_xpos=rotary_xpos,
+                attn_flash=attn_flash,
+                qk_norm=qk_norm,
+            )
+        )
 
-        except Exception as e:
-            print("Failed to initialize Andromeda: ", e)
-            raise
-
-    def forward(self, text, images, **kwargs):
-        """
-        Forward pass through the model. It expects the input text_tokens.
-        Args:
-        - text_tokens: Input tokens
-        - kwargs: Other arguments
-        Returns:
-        - output from the decoder
-        """
-        try:
-            images = self.clip_model(pixel_values=images)["last_hidden_state"]
-            # images = self.perceive(images).squeeze(1)
-            # images = self.image_proj(images)
-
-            model_input = self.decoder.forward(text, context=images)[0]
-            return self.decoder(model_input, padded_x=model_input[0])
-        except Exception as e:
-            print("Failed in forward method: ", e)
+    def forward(self, img, text):
+        try:    
+            encoded = self.encoder(img, return_embeddings=True)
+            return self.decoder(text, context=encoded)
+        except Exception as error:
+            print(f"Failed in forward method: {error}")
             raise
